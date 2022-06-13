78244e104f246d2a275141b9620eb07549712afc
olfa-dallel-enit <olfa.dallel@enit.utm.tn>
2022-06-13 08:36:41 +0000

    add

--- execution/chains/earth/configuration.sh
+++ execution/chains/earth/configuration.sh
@@ -7,7 +7,7 @@ crossdomaind query crossdomain show-root-certificate --node tcp://localhost:2665
 crossdomaind tx crossdomain create-forward-policy "broadcast" "" "" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
 crossdomaind query crossdomain show-forward-policy --node tcp://localhost:26657
 
-crossdomaind tx crossdomain  create-decision-policy 0 '' '' '{}' "" --from alice --yes --home ~/.mars --chain-id mars --node tcp://localhost:26659 --gas=auto --gas-adjustment=1.15
+crossdomaind tx crossdomain  create-decision-policy 0 '' '' '{}' "" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
 
 
 #create-update-policy [query] [event] [periodical-query]
@@ -15,5 +15,5 @@ crossdomaind tx cdac create-update-policy false true false --from alice --yes --
 crossdomaind query cdac show-update-policy --node tcp://localhost:26657
 
 #crossdomaind tx crossdomain create-cooperation-network-features [depth] [cost] [interest-list] [location-list] [last-update] [validity] [flags]
-crossdomaind tx crossdomain create-cooperation-network-features 0 0 "" "" "2022-06-12 16:00:00" '{"notBefore":"2023-01-01 00:00:00 +0000 UTC","notAfter":"2026-01-01 00:00:00 +0000 UTC"}' --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+crossdomaind tx crossdomain create-cooperation-network-features 0 0 "" "" "2022-01-01 00:00:00" '{"notBefore":"2023-01-01 00:00:00 +0000 UTC","notAfter":"2026-01-01 00:00:00 +0000 UTC"}' --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
 crossdomaind query crossdomain show-cooperation-network-features  --node tcp://localhost:26657
---
+++ execution/scenarios/cooperations.sh
@@ -0,0 +1,24 @@
+
+bash execution/chains/earth/configuration.sh
+
+#crossdomaind tx cdac create-domain-cooperation [label] [cooperation-type] [source-domain] [remote-domain] [validity] [interest] [cost] [creation-date] [creation-timestamp] [update-date] [update-timestamp] [status] [flags]
+crossdomaind tx cdac create-domain-cooperation "earth-mars.1" "Direct" '{"name":"earth","domainType":"Local","location":"Tunisie","Certificate":{},"ibc-connection":{}}' '{"name":"mars.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"notBefore":"2022-02-03 00:00:00","notAfter":"2024-02-03 00:00:00"}' "computing" 200 "2022-02-03 00:00:00" "1643846400000000000" "2022-02-03 00:00:00" "1643846400000000000" "Enabled" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+
+crossdomaind tx cdac create-domain-cooperation "mars.1-mars.2.1" "Indirect" '{"name":"mars.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"name":"mars.2.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"notBefore":"2022-02-03 00:00:00","notAfter":"2024-02-03 00:00:00"}' "computing" 200 "2022-02-03 00:00:00" "1643846400000000000" "2022-02-03 00:00:00" "1643846400000000000" "Enabled" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+
+crossdomaind tx cdac create-domain-cooperation "mars.1-mars.2.2" "Indirect" '{"name":"mars.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"name":"mars.2.2","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"notBefore":"2022-02-03 00:00:00","notAfter":"2024-02-03 00:00:00"}' "computing" 200 "2022-02-03 00:00:00" "1643846400000000000" "2022-02-03 00:00:00" "1643846400000000000" "Enabled" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+
+crossdomaind tx cdac create-domain-cooperation "mars.2.1-mars.3.1" "Indirect" '{"name":"mars.2.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"name":"mars.3.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"notBefore":"2022-02-03 00:00:00","notAfter":"2024-02-03 00:00:00"}' "computing" 200 "2022-02-03 00:00:00" "1643846400000000000" "2022-02-03 00:00:00" "1643846400000000000" "Enabled" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+
+crossdomaind tx cdac create-domain-cooperation "mars.3.1-mars.4.1" "Indirect" '{"name":"mars.3.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"name":"mars.4.1","domainType":"Remote","location":"france","Certificate":{},"ibc-connection":{}}' '{"notBefore":"2022-02-03 00:00:00","notAfter":"2024-02-03 00:00:00"}' "computing" 200 "2022-02-03 00:00:00" "1643846400000000000" "2022-02-03 00:00:00" "1643846400000000000" "Enabled" --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+
+
+
+crossdomaind query cdac list-domain-cooperation --node tcp://localhost:26657
+
+crossdomaind tx cdac generate-cooperation-network --from alice --yes --home ~/.earth --chain-id earth --node tcp://localhost:26657 --gas=auto --gas-adjustment=1.15
+crossdomaind query cdac list-cooperation-network --node tcp://localhost:26657
+
+
+
+
